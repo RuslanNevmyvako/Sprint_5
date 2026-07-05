552d7d75819c8a02a103abf0877cdfd6427f9555
Ruslan Nevmyvako <chilliwillirus@gmail.com>
2026-07-06 00:27:37 +0300

    Last changes

--- conftest.py
+++ conftest.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from constants import BASE_URL, EXISTING_USER
 from locators import HeaderLocators, LoginLocators
+from helpers import LoginSteps
 
 
 @pytest.fixture
@@ -21,9 +22,7 @@ def auth_driver(driver):
     """
     Предусловие: авторизованный пользователь.
     Возвращает драйвер с уже выполненным входом.
-    """
-    from helpers import LoginSteps
-    
+    """ 
     # Открываем модалку
     login_btn = WebDriverWait(driver, 5).until(
         expected_conditions.element_to_be_clickable(LoginLocators.LOGIN_BUTTON)
@@ -38,4 +37,4 @@ def auth_driver(driver):
         expected_conditions.presence_of_element_located(HeaderLocators.PROFILE_AVATAR)
     )
     
-    yield driver
+    return driver
